b5c3a99e1d05fa3fcd2c88d7918698d96d673384
SilasXIII <45399426+SilasXIII@users.noreply.github.com>
2021-05-23 18:45:28 +0200

    Add ImageView with Picasso

--- app/src/main/java/com/example/esiea_3a/presentation/detail/AnimeDetailFragment.kt
+++ app/src/main/java/com/example/esiea_3a/presentation/detail/AnimeDetailFragment.kt
@@ -6,17 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.RecyclerView
 import com.example.esiea_3a.R
+import com.squareup.picasso.Picasso
 
 
 class AnimeDetailFragment : Fragment() {
 
     private lateinit var textViewTitle: TextView
-    //private lateinit var textViewTitle: TextView
-    //private lateinit var textViewTitle: TextView
+    private lateinit var imgView: ImageView
+    private lateinit var textViewSynopsis: TextView
 
 
     override fun onCreateView(
@@ -34,6 +35,17 @@ class AnimeDetailFragment : Fragment() {
 
         textViewTitle = view.findViewById(R.id.anime_detail_title)
         textViewTitle.text = title
+
+        val img: String = arguments?.getString("image_url") ?: "no url"
+
+        imgView = view.findViewById(R.id.anime_detail_img)
+        Picasso.get().load(img).into(imgView)
+
+        val synopsis: String = arguments?.getString("synopsis") ?: "no synopsis"
+
+        textViewSynopsis = view.findViewById(R.id.anime_detail_synopsis)
+        textViewSynopsis.text = synopsis
+
 
         view.findViewById<Button>(R.id.button_back_to_list).setOnClickListener {
             findNavController().navigate(R.id.navigateToAnimeListFragment)
